0d46b504b3d898be43d7a7f3e58803fd8f7afc41
choisuji <choisuji0217@gmail.com>
2021-01-14 23:16:11 +0900

    commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,30 +12,66 @@ const routes = [
   { path: "/", name: "Home", component: () => <Home /> },
   { path: "/scroll", name: "scroll", component: () => <Scroll /> },
 
-  { path: "/css", name: "css" },
-  { path: "/React", name: "React" },
+  // { path: "/css", name: "css" },
+  // { path: "/React", name: "React" },
 
-  {
-    path: "/React package",
-    name: "React package",
-  },
+  // {
+  //   path: "/React package",
+  //   name: "React package",
+  // },
 ];
 
 
 function Routes() {
   var request = require("request");
-  var url = "http://naver.com";
+  var url = "https://www.google.com/";
   request(url, function(error, response, html){
     console.log(html);
   });
 
+  axios({
+    method: 'get',
+    url: 'https://www.google.com/',
+    headers: { 
+      // 'origin': 'http://javascript.info',
+      // 'Referer':'http://javascript.info/some/url'
+
+  // 'cache-control': 'no-cache'
+  // ,'content-encoding': 'gzip'
+  // ,'content-type': 'text/html;charset=EUC-KR'
+  // ,'expires': 'Thu, 01 Jan 1970 00:00:00 GMT'
+  // ,'referrer-policy': 'unsafe-url'
+  // ,'server': 'nfront'
+  // ,'set-cookie': 'JSESSIONID=35F2576E0E67E1EA290BD3AB721A8532; Path=/; HttpOnly'
+  // ,'strict-transport-security': 'max-age=31536000; includeSubDomains'
+  // ,'vary':' Accept-Encoding,User-Agent'
+  // ,'x-xss-protection': '1; mode=block'
+    },
+   })
+   .then(response => {      // .then : 응답(상태코드200~300미만)성공시
+      console.log(response);
+   })
+   .catch(error => {
+      console.log(error);
+   });
+  
+  
+
 //  axios({
 //   method: 'get',
 //   url: 'https://finance.naver.com/item/item_right_ajax.nhn?type=recent&code=005930&page=1',
-//   headers: { 'Content-Type': 'application/x-www-form-urlencoded',
-//   'Access-Control-Allow-Origin': '*',
-//   'Access-Control-Allow-Credentials': 'true' },
-
+//   headers: { 
+// 'cache-control': 'no-cache'
+// ,'content-encoding': 'gzip'
+// ,'content-type': 'text/html;charset=EUC-KR'
+// ,'expires': 'Thu, 01 Jan 1970 00:00:00 GMT'
+// ,'referrer-policy': 'unsafe-url'
+// ,'server': 'nfront'
+// ,'set-cookie': 'JSESSIONID=35F2576E0E67E1EA290BD3AB721A8532; Path=/; HttpOnly'
+// ,'strict-transport-security': 'max-age=31536000; includeSubDomains'
+// ,'vary':' Accept-Encoding,User-Agent'
+// ,'x-xss-protection': '1; mode=block'
+//   },
 //  })
 //  .then(response => {      // .then : 응답(상태코드200~300미만)성공시
 //     console.log(response);
@@ -43,6 +79,7 @@ function Routes() {
 //  .catch(error => {
 //     console.log(error);
 //  });
+
 
 
   return (
@@ -68,6 +105,8 @@ function Routes() {
 }
 
 export default Routes;
+
+
 
 
 
